b9adbdfd06eb25869d838ebe7f7800733ab8aadf
ol3gatron <olegatron@gmail.com>
2022-02-13 22:45:56 +0300

    visual fixes

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const buttons = document.querySelectorAll("button");
+const result = document.querySelector(".result");
+let abc = ""
 
-buttons.forEach(addEventListener("click", function() {
-  console.log("Click")
+buttons.forEach(addEventListener("click", function(e) {
+  console.log(e.target.innerText)
+  result.innerText = e.target.innerText;
 }))
