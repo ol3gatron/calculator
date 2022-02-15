7aa22867d8338dbb532642920cf3f66f404213e1
ol3gatron <olegatron@gmail.com>
2022-02-15 12:36:30 +0300

    completly redone. for now only basic functions working. A LOT OF BUGS!

--- script.js
+++ script.js
@@ -1,135 +1,100 @@
-const buttons = document.querySelectorAll(".digitsBtn");
-const funcBtns = document.querySelectorAll(".func")
-const result = document.querySelector(".result");
-const oper = document.querySelector(".operate")
+const digits = document.querySelectorAll(".digitsBtn")
+const operators = document.querySelectorAll(".func")
+const display = document.querySelector(".result")
+const result = document.querySelector(".operate")
+const clear = document.querySelector("#clear")
+const currentOperator = document.querySelector(".historyOutput")
 
-const clearBtn = document.querySelector("#clear")
+let num1 = ""
+let num2 = ""
+let num3 = ""
+let operator = ""
+let history = ""
 
-let arr = [];
-let arr2 = [];
-let funcArr = [];
-
-buttons.forEach((button) => {
-  button.addEventListener("click", function(e) {
-    if (funcArr.length === 0) {
-      result.innerText = e.target.innerText;
-      arr.push(e.target.innerText)
-      console.log(arr)
-    } else {
-      result.innerText = e.target.innerText;
-      arr2.push(e.target.innerText)
-      console.log(arr2)
-    }
-  })
-})
-
-funcBtns.forEach((button) => {
-  button.addEventListener("click", function(e) {
-    funcArr = []
-    result.innerText = e.target.innerText;
-    funcArr.push(e.target.innerText)
-    console.log(funcArr)
-    check()
-  })
-})
-
-function check() {
-  let tempArr = [];
-  let temp;
-  for (let i = 0; i < arr.length; i++) {
-    if (!isNaN(parseInt(arr[i]))) {
-      tempArr += arr[i]
-    } else {
-      break
-    }
-  }
-  arr = [];
-  arr[0] = tempArr;
-}
-
-function check2() {
-  let tempArr = [];
-  for (let i = 0; i < arr2.length; i++) {
-    tempArr += arr2[i]
-  }
-  arr2 = [];
-  arr2[0] = tempArr;
-}
-
-oper.addEventListener("click", function() {
-  check2();
-  if (funcArr[0] === "×") {
-    multiply()
-  } else if (funcArr[0] === "÷") {
-    divide()
-  } else if (funcArr[0] === "-") {
-    minus()
-  } else if (funcArr[0] === "+") {
-    plus()
-  } else if (funcArr[0] === "nn" || funcArr[0] === "n") {
-    power()
-  } else if (funcArr[0] === "fib(n)") {
-    fib()
-  } else if (funcArr[0] === "n!") {
-    factorial()
-  }
-  arr = []
-  arr2 = []
-  arr[0] = result.innerText
-})
-
-function multiply() {
-  result.innerText = arr[0] * arr2[0]
-}
-
-function divide() {
-  result.innerText = arr[0] / arr2[0]
-}
-
-function minus() {
-  result.innerText = arr[0] - arr2[0]
-}
-
-function plus() {
-  result.innerText = +arr[0] + +arr2[0]
-}
-
-function power() {
-  result.innerText = arr[0] ** arr2[0]
-}
-
-function fib() {
-  console.log("asa")
-  if (arr[0] < 0) {
-    return "OOPS"
-  } else if (arr[0] == 1) {
-    return 1
+result.addEventListener("click", () => {
+  if (num1 == 0 || num2 == 0 && operator === "÷") {
+    display.innerText = "Ты не можешь разделить на ноль, дурень!"
   } else {
-    let temp1 = 0;
-    let temp2 = 1
-    let result2 = 0;
-    for (let i = 1; i < arr[0]; i++) {
-      result2 = temp1 + temp2;
-      temp1 = temp2;
-      temp2 = result2;
+    num3 = operate(operator, num1, num2)
+    .innerText = num3
+    num1 = num3;
+    console.log(`Третье число: ${num3}`)
+    num2 = "";
+    num3 = "";
+  }
+})
+
+function operate(operator, num1, num2) {
+  history = ""
+  history += num1;
+  history += operator;
+  history += num2;
+  currentOperator.innerText = history
+  if (operator === "+") {
+    return add(parseInt(num1), parseInt(num2))
+  } else if (operator === "-") {
+    return substract(num1, num2)
+  } else if (operator === "*") {
+    return multiply(num1, num2)
+  } else if (operator === "÷") {
+    return divide(num1, num2)
+  }
+}
+
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function substract(num1, num2) {
+  return num1 - num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2) {
+  return num1 / num2
+}
+
+digits.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (operator) {
+      num2 += e.target.innerText
+      display.innerText = num2
+      console.log(`Второе число: ${num2}`)
+    } else {
+      num1 += e.target.innerText
+      display.innerText = num1
+      console.log(`Первое число: ${num1}`)
     }
-    result.innerText = result2;
-  }
-}
+  })
+})
 
-function factorial() {
-  let resultFac = 1
-  for (let i = 0; i = arr[0]; i++) {
-    resultFac *= i
-  }
-  result.textContent = resultFac;
-}
+operators.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (num1 && num2) {
+      console.log(num1 + " " + num2)
+      num3 = operate(operator, num1, num2)
+      display.innerText = num3
+      num1 = num3;
+      console.log(`Третье число: ${num3}`)
+      num2 = "";
+      num3 = "";
+    }
+    operator = e.target.innerText
+    console.log(operator)
+  })
+})
 
-
-
-clearBtn.addEventListener("click", function() {
-  arr = []
-  arr2 = []
-  funcArr = []
-  result.innerText = 0
+clear.addEventListener("click", () => {
+  num1 = ""
+  num2 = ""
+  num3 = ""
+  operator = ""
+  display.innerText = 0
+  currentOperator.innerText = ""
+  history = ""
+  console.log("Clear")
+  console.clear()
 })
